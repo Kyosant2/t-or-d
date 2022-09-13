41192a9a58d6126d27b376dc2bbc2bdd5764a301
Kyosant2 <112980519+Kyosant2@users.noreply.github.com>
2022-09-13 12:11:14 -0600

    added everything

--- main.ts
+++ main.ts
@@ -1,3 +1,47 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    TruthORDare = randint(0, 1)
+    if (TruthORDare == 0) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("Dare")
+    }
 })
+input.onButtonPressed(Button.AB, function () {
+    arrow = randint(0, 3)
+    if (TruthORDare == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (TruthORDare == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (TruthORDare == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . . . .
+            `)
+    }
+})
+let arrow = 0
+let TruthORDare = 0
+basic.showString("WELCOME TO TRUTH OR DARE... ")
